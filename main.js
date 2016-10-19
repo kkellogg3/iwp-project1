3987ef9e24baf79c9acb03614c5a5da404728abf
Kendall Kellogg <kkellogg3@mail.valenciacollege.edu>
2016-10-19 14:22:52 +0000

    Project 1

---
+++ main.js
@@ -0,0 +1,324 @@
+function gradeQuiz() {
+	
+	var userScore = 0;
+	
+	
+/* Question 1 Code */
+	var answerOne = document.querySelector('input[name="q1"]:checked').value;
+	console.log("You chose" + answerOne);
+
+	if(answerOne == 2) {
+		userScore += 1;
+	} else {
+	 console.log("Incorrect");
+	}
+	console.log("Your score is" + userScore);	
+
+/* Question 2 Code */
+	var answerTwo = document.querySelector('input[name="q2"]:checked').value;
+	console.log("You chose" + answerTwo);
+
+	if(answerTwo == 4) {
+		userScore +=1;
+	} else {
+		console.log("Incorrect");
+	}
+	console.log("Your score is" + userScore);
+
+	/* Question 3 Code */
+	var answerThree = document.querySelector('input[name="q3"]:checked').value;
+	console.log("You chose" + answerThree);
+
+	if(answerThree == 3) {
+		userScore +=1;
+	} else {
+		console.log("Incorrect");
+	}
+	console.log("Your score is" + userScore);
+
+	/* Question 4 Code */
+	var answerFour = document.querySelector('input[name="q4"]:checked').value;
+	console.log("You chose" + answerFour);
+
+	if(answerFour == 2) {
+		userScore +=1;
+	} else {
+		console.log("Incorrect");
+	}
+	console.log("Your score is" + userScore);
+
+	/* Question 5 Code */
+	var answerFive = document.querySelector('input[name="q5"]:checked').value;
+	console.log("You chose" + answerFive);
+
+	if(answerFive == 1) {
+		userScore +=1;
+	} else {
+		console.log("Incorrect");
+	}
+	console.log("Your score is" + userScore);
+
+	/* Question 6 Code */
+	var answerSix = document.querySelector('input[name="q6"]:checked').value;
+	console.log("You chose" + answerSix);
+
+	if(answerSix == 3) {
+		userScore +=1;
+	} else {
+		console.log("Incorrect");
+	}
+	console.log("Your score is" + userScore);
+
+/* Question 7 Code */
+	var answerSeven = document.querySelector('input[name="q7"]:checked').value;
+	console.log("You chose" + answerTwo);
+
+	if(answerSeven == 2) {
+		userScore +=1;
+	} else {
+		console.log("Incorrect");
+	}
+	console.log("Your score is" + userScore);
+
+	/* Question 8 Code */
+	var answerEight = document.querySelector('input[name="q8"]:checked').value;
+	console.log("You chose" + answerEight);
+
+	if(answerEight == 3) {
+		userScore +=1;
+	} else {
+		console.log("Incorrect");
+	}
+	console.log("Your score is" + userScore);
+
+	/* Question 9 Code */
+	var answerNine = document.querySelector('input[name="q9"]:checked').value;
+	console.log("You chose" + answerNine);
+
+	if(answerNine == 4) {
+		userScore +=1;
+	} else {
+		console.log("Incorrect");
+	}
+	console.log("Your score is" + userScore);
+
+	/* Question 10 Code */
+	var answerTen = document.querySelector('input[name="q10"]:checked').value;
+	console.log("You chose" + answerTen);
+
+	if(answerTen == 1) {
+		userScore +=1;
+	} else {
+		console.log("Incorrect");
+	}
+	console.log("Your score is" + userScore);
+
+	/* Question 11 Code */
+	var answerEleven = document.querySelector('input[name="q11"]:checked').value;
+	console.log("You chose" + answerEleven);
+
+	if(answerEleven == 2) {
+		userScore +=1;
+	} else {
+		console.log("Incorrect");
+	}
+	console.log("Your score is" + userScore);
+
+	/* Question 12 Code */
+	var answerTwelve = document.querySelector('input[name="q12"]:checked').value;
+	console.log("You chose" + answerTwelve);
+
+	if(answerTwelve == 2) {
+		userScore +=1;
+	} else {
+		console.log("Incorrect");
+	}
+	console.log("Your score is" + userScore);
+
+	/* Question 13 Code */
+	var answerThirteen = document.querySelector('input[name="q13"]:checked').value;
+	console.log("You chose" + answerThirteen);
+
+	if(answerThirteen == 1) {
+		userScore +=1;
+	} else {
+		console.log("Incorrect");
+	}
+	console.log("Your score is" + userScore);
+
+	/* Question 14 Code */
+	var answerFourteen = document.querySelector('input[name="q14"]:checked').value;
+	console.log("You chose" + answerFourteen);
+
+	if(answerFourteen == 2) {
+		userScore +=1;
+	} else {
+		console.log("Incorrect");
+	}
+	console.log("Your score is" + userScore);
+
+	/* Question 15 Code */
+	var answerFifteen = document.querySelector('input[name="q15"]:checked').value;
+	console.log("You chose" + answerFifteen);
+
+	if(answerFifteen == 4) {
+		userScore +=1;
+	} else {
+		console.log("Incorrect");
+	}
+	console.log("Your score is" + userScore);
+
+	/* Question 16 Code */
+	var answerSixteen = document.querySelector('input[name="q16"]:checked').value;
+	console.log("You chose" + answerSixteen);
+
+	if(answerSixteen == 3) {
+		userScore +=1;
+	} else {
+		console.log("Incorrect");
+	}
+	console.log("Your score is" + userScore);
+
+	/* Question 17 Code */
+	var answerSeventeen = document.querySelector('input[name="q17"]:checked').value;
+	console.log("You chose" + answerSeventeen);
+
+	if(answerSeventeen == 1) {
+		userScore +=1;
+	} else {
+		console.log("Incorrect");
+	}
+	console.log("Your score is" + userScore);
+
+	/* Question 18 Code */
+	var answerEighteen = document.querySelector('input[name="q18"]:checked').value;
+	console.log("You chose" + answerEighteen);
+
+	if(answerEighteen == 4) {
+		userScore +=1;
+	} else {
+		console.log("Incorrect");
+	}
+	console.log("Your score is" + userScore);
+
+	/* Question 19 Code */
+	var answerNineteen = document.querySelector('input[name="q19"]:checked').value;
+	console.log("You chose" + answerNineteen);
+
+	if(answerNineteen == 1) {
+		userScore +=1;
+	} else {
+		console.log("Incorrect");
+	}
+	console.log("Your score is" + userScore);
+
+	/* Question 20 Code */
+	var answerTwenty = document.querySelector('input[name="q20"]:checked').value;
+	console.log("You chose" + answerTwenty);
+
+	if(answerTwenty == 2) {
+		userScore +=1;
+	} else {
+		console.log("Incorrect");
+	}
+	console.log("Your score is" + userScore);
+
+	/* Question 21 Code */
+	var answerTwentyOne = document.querySelector('input[name="q21"]:checked').value;
+	console.log("You chose" + answerTwentyOne);
+
+	if(answerTwentyOne == 3) {
+		userScore +=1;
+	} else {
+		console.log("Incorrect");
+	}
+	console.log("Your score is" + userScore);
+
+	/* Question 22 Code */
+	var answerTwentyTwo = document.querySelector('input[name="q22"]:checked').value;
+	console.log("You chose" + answerTwentyTwo);
+
+	if(answerTwentyTwo == 4) {
+		userScore +=1;
+	} else {
+		console.log("Incorrect");
+	}
+	console.log("Your score is" + userScore);
+
+	/* Question 23 Code */
+	var answerTwentyThree = document.querySelector('input[name="q23"]:checked').value;
+	console.log("You chose" + answerTwentyThree);
+
+	if(answerTwentyThree == 4) {
+		userScore +=1;
+	} else {
+		console.log("Incorrect");
+	}
+	console.log("Your score is" + userScore);
+
+	/* Question 24 Code */
+	var answerTwentyFour = document.querySelector('input[name="q24"]:checked').value;
+	console.log("You chose" + answerTwentyFour);
+
+	if(answerTwentyFour == 1) {
+		userScore +=1;
+	} else {
+		console.log("Incorrect");
+	}
+	console.log("Your score is" + userScore);
+
+/* Question 25 Code */
+	var answerTwentyFive = document.querySelector('input[name="q25"]:checked').value;
+	console.log("You chose" + answerTwentyFive);
+
+	if(answerTwentyFive == 2) {
+		userScore +=1;
+	} else {
+		console.log("Incorrect");
+	}
+	console.log("Your score is" + userScore);
+
+	/* Question 26 Code */
+	var answerTwentySix = document.querySelector('input[name="q26"]:checked').value;
+	console.log("You chose" + answerTwentySix);
+
+	if(answerTwentySix == 4) {
+		userScore +=1;
+	} else {
+		console.log("Incorrect");
+	}
+	console.log("Your score is" + userScore);
+
+	/* Question 27 Code */
+	var answerTwentySeven = document.querySelector('input[name="q27"]:checked').value;
+	console.log("You chose" + answerTwentySeven);
+
+	if(answerTwentySeven == 1) {
+		userScore +=1;
+	} else {
+		console.log("Incorrect");
+	}
+	console.log("Your score is" + userScore);
+
+	/* Question 28 Code */
+	var answerTwentyEight = document.querySelector('input[name="q28"]:checked').value;
+	console.log("You chose" + answerTwentyEight);
+
+	if(answerTwentyEight == 2) {
+		userScore +=1;
+	} else {
+		console.log("Incorrect");
+	}
+	console.log("Your score is" + userScore);
+
+if (userScore == 28) {
+	document.getElementById("userResult").innerHTML = "You're an American History Expert!";
+} else if (userScore >= 21) {
+	document.getElementById("userResult").innerHTML = "You know your stuff!";
+} else if (userScore >= 14) {
+	document.getElementById("userResult").innerHTML = "You might want to do some studying.";
+} else if (userScore == 0) {
+	document.getElementById("userResult").innerHTML = "Better luck next time!";
+}
+
+}
